6bdb769b900162f985a791c133c9d7714cc5a740
Sujay Joshi <joshi.suj@husky.neu.edu>
2019-12-01 21:34:31 -0500

    app spec final chnages7

--- App_spec_scripts/install_dependencies.sh
+++ App_spec_scripts/install_dependencies.sh
@@ -1,6 +1,1 @@
-sudo chmod 777 /home/ec2-user/App/
-sudo chmod 777 /home/ec2-user/App/backend/ 
-cd /home/ec2-user/App/backend/ 
-~/.nvm/versions/node/v13.2.0/bin/npm install >> 
-~/.nvm/versions/node/v13.2.0/bin/npm install node-sass
-~/.nvm/versions/node/v13.2.0/bin/npm audit fix
+
--- App_spec_scripts/start_server.sh
+++ App_spec_scripts/start_server.sh
@@ -1,2 +1,8 @@
+sudo chmod 777 /home/ec2-user/App/
+sudo chmod 777 /home/ec2-user/App/backend/ 
+cd /home/ec2-user/App/backend/ 
+~/.nvm/versions/node/v13.2.0/bin/npm install >> 
+~/.nvm/versions/node/v13.2.0/bin/npm install node-sass
+~/.nvm/versions/node/v13.2.0/bin/npm audit fix
 cd /home/ec2-user/App/backend/
 ~/.nvm/versions/node/v13.2.0/bin/npm start
