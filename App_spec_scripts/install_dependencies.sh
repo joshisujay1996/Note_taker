d09cb506bb176d87bf9a88e925a777a8eaf5847f
Sujay Joshi <joshi.suj@husky.neu.edu>
2019-12-01 21:08:31 -0500

    app spec final chnages3

--- App_spec_scripts/install_dependencies.sh
+++ App_spec_scripts/install_dependencies.sh
@@ -1,6 +1,7 @@
-sudo chmod 777 /home/ec2-user/App/
-sudo chmod 777 /home/ec2-user/App/backend/
+mkdir logs
+sudo chmod 777 /home/ec2-user/App/ 
+sudo chmod 777 /home/ec2-user/App/backend/ >> /home/ec2-user/logs/logs1.txt
 cd /home/ec2-user/App/backend/
-npm install
-npm install node-sass
-npm audit fix
+npm install >> /home/ec2-user/logs/logs2.txt
+npm install node-sass >> /home/ec2-user/logs/logs3.txt
+npm audit fix >> /home/ec2-user/logs/logs4.txt
