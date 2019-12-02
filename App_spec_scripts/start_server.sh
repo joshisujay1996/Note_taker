a29c93018c4aec9df816c456db7926b03d5eea78
Sujay Joshi <joshi.suj@husky.neu.edu>
2019-12-01 21:50:10 -0500

    app spec final chnages8

--- App_spec_scripts/start_server.sh
+++ App_spec_scripts/start_server.sh
@@ -1,8 +1,15 @@
+sudo chown ec2-user /home/ec2-user/App/
 sudo chmod 777 /home/ec2-user/App/
 sudo chmod 777 /home/ec2-user/App/backend/ 
 cd /home/ec2-user/App/backend/ 
-~/.nvm/versions/node/v13.2.0/bin/npm install >> 
-~/.nvm/versions/node/v13.2.0/bin/npm install node-sass
-~/.nvm/versions/node/v13.2.0/bin/npm audit fix
+
+if [ "$(whoami)" != "ec2-user" ]; then
+        echo "Script must be run as user: username"
+        exit -1
+fi
+
+npm install 
+npm install node-sass
+npm audit fix
 cd /home/ec2-user/App/backend/
-~/.nvm/versions/node/v13.2.0/bin/npm start
+npm start
